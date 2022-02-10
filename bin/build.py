1a11cdd8c573f6463ba9e46921f3a92ccc637ba1
Joel Palmius <joepal1976@hotmail.com>
2022-02-10 14:28:57 +0100

    Build utility

---
+++ bin/build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from _general import find_hugo, chdir_to_root
+import subprocess, os
+
+hugo = find_hugo()
+
+chdir_to_root()
+subprocess.run(["rm -rf dist/* build/*"], shell=True)
+subprocess.run([hugo, "-D"])
+os.chdir("build")
+subprocess.run(["tar", "cvzf", "../dist/static.tgz", "."])
+
